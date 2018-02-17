3bc807bd08d7e284577b04bb26677de28ba19c93
Matthias Grunewald <matthias.grunewald@gritplex.com>
2018-02-17 10:16:20 +0100

    added CSharpScripting solution

---
+++ CSharpScripting/ConsoleTestClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
