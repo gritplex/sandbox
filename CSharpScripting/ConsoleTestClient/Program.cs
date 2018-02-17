dd2affb5f882cdb1fe039f302f710c48e93f84b4
Matthias Grunewald <matthias.grunewald@gritplex.com>
2018-02-17 10:44:07 +0100

    basically a simple calculator

--- CSharpScripting/ConsoleTestClient/Program.cs
+++ CSharpScripting/ConsoleTestClient/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using ScriptingBase;
+using System;
+using System.Threading.Tasks;
 
 namespace ConsoleTestClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var scriptExecuter = new Script();
+            string input = "";
+            bool quitRequested = false;
+
+            Console.WriteLine("Type an expression to be evaluated or 'quit' to exit.");
+            do
+            {
+                input = Console.ReadLine();
+                quitRequested = string.Equals(input, "quit", StringComparison.OrdinalIgnoreCase);
+                if (!quitRequested)
+                {
+                    object result = await scriptExecuter.ExecuteAsync(input);
+                    Console.WriteLine(scriptExecuter.Format(result));
+                }
+            } while (!quitRequested);
         }
     }
 }
---
+++ CSharpScripting/ScriptingBase/Script.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting.Hosting;
+
+namespace ScriptingBase
+{
+    /*
+        This is a test class for getting familiar with the Scripting API offered by Roslyn.
+        Test things here!
+    */
+
+    public class Script
+    {
+        public string Format(object obj)
+        {
+            switch (obj)
+            {
+                case Exception e:
+                    return CSharpObjectFormatter.Instance.FormatException(e);
+                default:
+                    return CSharpObjectFormatter.Instance.FormatObject(obj);
+            }
+        }
+
+        public async Task<object> ExecuteAsync(string expression)
+        {
+            object result;
+
+            try
+            {
+                result = await CSharpScript.EvaluateAsync(expression);
+            }
+            catch (Exception e)
+            {
+                result = e;                
+            }
+
+            return result;
+        }
+    }
+}
